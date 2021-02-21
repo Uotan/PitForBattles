bd6eaf0de6a065ce1f5a8a9ef6027733b8ee4841
Ddmkovich <rflntgrn@gmail.com>
2021-02-21 20:57:31 +0700

    Поменял уничтожение пули

--- Assets/C#/ShotgunBullet.cs
+++ Assets/C#/ShotgunBullet.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,17 +7,18 @@ public class ShotgunBullet : MonoBehaviour
     public float speedXmin;
     public float speedXmax;
     public float lifetime;
-    public float distance;
     public int damage;
-    public LayerMask WhatIsSolid;
     public float YRangePlus;
     public float YRangeMinus;
     [HideInInspector]
     public float Y;
+    [HideInInspector]
     public float X;
 
+
     private (float X, float Y) Start()
-    {       float randX = Random.Range(speedXmin,speedXmax);
+    {       
+            float randX = Random.Range(speedXmin,speedXmax);
              X = randX;
             float randY = Random.Range(-YRangeMinus, YRangePlus);
              Y = randY; 
@@ -27,28 +28,32 @@ public class ShotgunBullet : MonoBehaviour
     }
 
     private void Update()
-    {
-        RaycastHit2D raycastHit2D = Physics2D.Raycast(transform.position, transform.up, distance, WhatIsSolid);
-        if (raycastHit2D.collider != null)
-        {
-            if (raycastHit2D.collider.CompareTag("Enemy"))
-            {
-                raycastHit2D.collider.GetComponent<Enemy>().TakeDamage(damage);
-
-            }
-            Destroy(gameObject);
-
-        }
-        
+    { 
         if (lifetime <= 0)
         {
             Destroy(gameObject);
         }
         lifetime -= Time.deltaTime;
         
+
         transform.Translate(-X * Time.deltaTime,Y*Time.deltaTime, 0);
-
-
-
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Bullet"))
+        {
+
+        }
+        else
+        {
+            if (other.CompareTag("Enemy"))
+            {
+                other.GetComponent<Enemy>().TakeDamage(damage);
+            }
+            Destroy(gameObject);
+        }
+    }
+
+
 }
