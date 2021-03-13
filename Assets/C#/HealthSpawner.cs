940464b44b17436d4eb647c74e9fda6794e2df1a
Ddmkovich <rflntgrn@gmail.com>
2021-03-14 01:02:19 +0700

    *Переделал спавнер Теперь токи спавно нужно заносить в массив startSpawnPoint который затем присваивается spawnPoint spawnPoint изменяется по мере спавна объекта, а конкретно удаляется та точка, в которой заспавнился объект, тоесть если объект заспавнился в точке 1 (все точек 5), то точек свободного спавна осталось 4:2345 когда весь список spawnPoint удалён он перезаписывается заново проблема когда объект спавнится 2 раза в одной точке например при случае когда остаётся точка 5, следующей точкой спавна будет точка 1 (после перезаписи массива),решил условием на количество объектов на сцене, теперь спавн 2 объектов происходит только при отсутствии этих объектов на сцене вообще if(healthObject==0) Также переписал корутину на Update как в остальных скриптах потому что баклажан

--- Assets/C#/HealthSpawner.cs
+++ Assets/C#/HealthSpawner.cs
@@ -1,53 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Linq;
+using System;
 public class HealthSpawner : MonoBehaviour
-{
-    public GameObject health1;
-    public GameObject health2;
-    public GameObject health3;
-    public GameObject health4;
-    public GameObject health5;
+{   
+    public GameObject[] startSpawnPoint;
+    public List<GameObject> spawnPoint;
+    
+
     public GameObject PrefabHealth;
 
-    private int Ran;
-    public float Time;
+    public float startSpawnTime;
+    public float spawnTime;
+
+    private int[] Ran;
     void Start()
     {
-        StartCoroutine(CheckHealth());
+        for (int i = 0; i < startSpawnPoint.Length; i++)
+        {
+            spawnPoint.Add(startSpawnPoint[i]);
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    }
-    IEnumerator CheckHealth()
-    {
-        while (true)
+        if (spawnTime<=0)
         {
-            yield return new WaitForSeconds(Time);
             var healthObjects = GameObject.FindGameObjectsWithTag("Health");
-            if (healthObjects.Length < 2)
+            if (healthObjects.Length==0)
             {
-                CheckAndSpawn();
+                CheckHealth();
+            }
+            spawnTime = startSpawnTime;
+        }
+        else
+        {
+            spawnTime -= UnityEngine.Time.deltaTime;
+        }
+    }
+    public void CheckHealth()
+    {
+
+
+        var healthObjects = GameObject.FindGameObjectsWithTag("Health");
+        while (healthObjects.Length<2)
+        {
+            if (spawnPoint.Count <= 1)
+            {
+                for (int j = 0; j < startSpawnPoint.Length; j++)
+                {
+                    spawnPoint.Add(startSpawnPoint[j]);
+                }
+            }
+            int lastEnumerable = spawnPoint.Count;
+            System.Random random = new System.Random();
+            Ran = Enumerable.Range(0, lastEnumerable).OrderBy(i => random.Next()).ToArray();
+            for (int i = 0; i < spawnPoint.Count; i++)
+            {   
+                
+                if (Ran[i] == i)
+                {
+                    Instantiate(PrefabHealth, spawnPoint[i].transform.position, Quaternion.identity);
+                    spawnPoint.RemoveAt(i);
+                    
+                }
+                healthObjects = GameObject.FindGameObjectsWithTag("Health");
             }
         }
-
-    }
-    void CheckAndSpawn()
-    {
-        Ran = Random.Range(1, 6);
-        switch (Ran)
-        {
-            case 1: Instantiate(PrefabHealth, health1.transform.position, Quaternion.identity); break;
-            case 2: Instantiate(PrefabHealth, health2.transform.position, Quaternion.identity); break;
-            case 3: Instantiate(PrefabHealth, health3.transform.position, Quaternion.identity); break;
-            case 4: Instantiate(PrefabHealth, health4.transform.position, Quaternion.identity); break;
-            case 5: Instantiate(PrefabHealth, health5.transform.position, Quaternion.identity); break;
-            default:
-                break;
-        }
+             
+        
     }
 }
